6e39781e03005cb0ad7cee4ad3a93be844bda3a9
vvv08 <venusvasu08@gmail.com>
2024-05-15 18:48:10 +0530

    Error Page

--- client/src/components/maintenance/index.jsx
+++ client/src/components/maintenance/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import './maintenance.scss';
 
 const Maintenance = () => {
   return (
     <>
-        Maintenance
+      <div className="error-container">
+        <div className="error-code">Server Error</div>
+        <div className="error-message">Requested page has faced an unexpected error</div>
+        <div className="error-description">
+          The page you are looking for might have been removed, had its name
+          Please raise the issue to ensure smooth functionality.
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Maintenance
+export default Maintenance;
