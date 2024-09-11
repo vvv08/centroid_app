b56fa8b39ecd5a565bec838eeeced0067e14049d
vvv08 <venusvasu08@gmail.com>
2024-09-11 18:01:37 +0530

    capa current date fix

--- server/controllers/customerRejections/capa.js
+++ server/controllers/customerRejections/capa.js
@@ -1,18 +1,18 @@
 import { db } from "../../config/connection.js";
-import { padZero } from "../../validations/validations.js";
+// import { padZero } from "../../validations/validations.js";
 
-//To get current date
-const currentDate = new Date();
-const istOffset = 5.5 * 60 * 60 * 1000;
-const istDate = new Date(currentDate.getTime() + istOffset);
-const year = currentDate.getFullYear();
-const month = padZero(istDate.getMonth() + 1); // Months are zero-based (0 = January)
-const day = padZero(istDate.getDate());
-const hours = padZero(istDate.getUTCHours());
-const minutes = padZero(istDate.getUTCMinutes());
-const seconds = padZero(istDate.getUTCSeconds());
-// const curr_date = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-const curr_date = `${year}-${month}-${day}`;
+// //To get current date
+// const currentDate = new Date();
+// const istOffset = 5.5 * 60 * 60 * 1000;
+// const istDate = new Date(currentDate.getTime() + istOffset);
+// const year = currentDate.getFullYear();
+// const month = padZero(istDate.getMonth() + 1); // Months are zero-based (0 = January)
+// const day = padZero(istDate.getDate());
+// const hours = padZero(istDate.getUTCHours());
+// const minutes = padZero(istDate.getUTCMinutes());
+// const seconds = padZero(istDate.getUTCSeconds());
+// // const curr_date = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+// const curr_date = `${year}-${month}-${day}`;
 
 export const getCAPADetails = ({ id }) => {
   return new Promise(async (resolve, reject) => {
@@ -21,23 +21,23 @@ export const getCAPADetails = ({ id }) => {
       const [general] = await db.query(`select c.name as customer, i.invoice_number as invoice_number, i.invoice_id as invoice_id, json_arrayagg(w.work_order) as work_order, json_arrayagg(p.part_number) as part_number, json_arrayagg(p.part_name) as part_name from invoices i inner join invoice_work_order iw on i.invoice_id = iw.invoice_id inner join work_orders w on iw.work_order_id = w.work_order_id inner join part_numbers p on w.part_number_id = p.part_number_id inner join customers c on i.customer_id = c.customer_id where i.invoice_id = ${id} group by iw.invoice_id;`);
 
       const [issues] = await db.query(
-        `select u.description as uom , r.cust_rej_id, i.invoice_number, r.description as problem,  date_format(r.created_date, '%Y-%m-%d') as created_date, date_format(r.last_updated, '%Y-%m-%d') as last_updated , r.rejected_qty from cust_rejection_issues r inner join invoices i on r.invoice_id = i.invoice_id inner join unit_of_measurements u on r.uom_id = u.uom_id where r.invoice_id = ${id};`
+        `select u.description as uom , r.cust_rej_id, i.invoice_number, r.description as problem,  date_format(CONVERT_TZ(r.created_date, '+00:00', '+05:30'), '%Y-%m-%d') as created_date, date_format(CONVERT_TZ(r.last_updated, '+00:00', '+05:30'), '%Y-%m-%d') as last_updated , r.rejected_qty from cust_rejection_issues r inner join invoices i on r.invoice_id = i.invoice_id inner join unit_of_measurements u on r.uom_id = u.uom_id where r.invoice_id = ${id};`
       );
 
       const [containment] = await db.query(
-        `select  date_format(c.created_date, '%Y-%m-%d') as created_date, date_format(c.last_updated, '%Y-%m-%d') as last_updated  ,c.containment_id, c.stock_check_supplier, date_format(c.supplier_date, '%Y-%m-%d') as supplier_date, c.stock_check_customer , date_format(c.customer_date, '%Y-%m-%d') as customer_date , c.stock_check_production , date_format(c.production_date, '%Y-%m-%d') as production_date, c.stock_check_transit , date_format(c.transit_date, '%Y-%m-%d') as transit_date, c.remarks , r.description as problem from cust_containment_actions c inner join cust_rejection_issues r on c.cust_rej_id = r.cust_rej_id where c.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
+        `select  date_format(CONVERT_TZ(c.created_date, '+00:00', '+05:30'), '%Y-%m-%d') as created_date, date_format(CONVERT_TZ(c.last_updated, '+00:00', '+05:30'), '%Y-%m-%d') as last_updated  ,c.containment_id, c.stock_check_supplier, date_format(c.supplier_date, '%Y-%m-%d') as supplier_date, c.stock_check_customer , date_format(c.customer_date, '%Y-%m-%d') as customer_date , c.stock_check_production , date_format(c.production_date, '%Y-%m-%d') as production_date, c.stock_check_transit , date_format(c.transit_date, '%Y-%m-%d') as transit_date, c.remarks , r.description as problem from cust_containment_actions c inner join cust_rejection_issues r on c.cust_rej_id = r.cust_rej_id where c.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
       );
 
       const [root_causes] = await db.query(
-        `select rt.root_id, rt.description as root_cause, rt.remarks ,  date_format(rt.created_date, '%Y-%m-%d') as created_date, date_format(rt.last_updated, '%Y-%m-%d') as last_updated  , r.description as problem from cust_root_causes rt inner join cust_rejection_issues r on rt.cust_rej_id = r.cust_rej_id where rt.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
+        `select rt.root_id, rt.description as root_cause, rt.remarks ,  date_format(CONVERT_TZ(rt.created_date, '+00:00', '+05:30'), '%Y-%m-%d') as created_date, date_format(CONVERT_TZ(rt.last_updated, '+00:00', '+05:30'), '%Y-%m-%d') as last_updated  , r.description as problem from cust_root_causes rt inner join cust_rejection_issues r on rt.cust_rej_id = r.cust_rej_id where rt.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
       );
 
       const [corrective_actions] = await db.query(
-        `select c.corrective_id , c.description as corrective_action ,  date_format(c.created_date, '%Y-%m-%d') as created_date, date_format(c.last_updated, '%Y-%m-%d') as last_updated  , c.remarks , i.name as inspector , rt.description as problem from cust_corrective_actions c inner join cust_rejection_issues rt on c.cust_rej_id = rt.cust_rej_id inner join inspectors i on c.inspector_id = i.inspector_id where c.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
+        `select c.corrective_id , c.description as corrective_action ,  date_format(CONVERT_TZ(c.created_date, '+00:00', '+05:30'), '%Y-%m-%d') as created_date, date_format(CONVERT_TZ(c.last_updated, '+00:00', '+05:30'), '%Y-%m-%d') as last_updated  , c.remarks , i.name as inspector , rt.description as problem from cust_corrective_actions c inner join cust_rejection_issues rt on c.cust_rej_id = rt.cust_rej_id inner join inspectors i on c.inspector_id = i.inspector_id where c.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
       );
 
       const [preventive_actions] = await db.query(
-        `select p.preventive_id , p.description as preventive_action ,  date_format(p.created_date, '%Y-%m-%d') as created_date, date_format(p.last_updated, '%Y-%m-%d') as last_updated  , p.remarks , i.name as inspector , r.description as problem from cust_preventive_actions p inner join cust_rejection_issues r on p.cust_rej_id = r.cust_rej_id inner join inspectors i on p.inspector_id = i.inspector_id where p.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
+        `select p.preventive_id , p.description as preventive_action ,  date_format(CONVERT_TZ(p.created_date, '+00:00', '+05:30'), '%Y-%m-%d') as created_date, date_format(CONVERT_TZ(p.last_updated, '+00:00', '+05:30'), '%Y-%m-%d') as last_updated  , p.remarks , i.name as inspector , r.description as problem from cust_preventive_actions p inner join cust_rejection_issues r on p.cust_rej_id = r.cust_rej_id inner join inspectors i on p.inspector_id = i.inspector_id where p.cust_rej_id in (select cust_rej_id from cust_rejection_issues where invoice_id = ${id} group by cust_rej_id);`
       );
 
       resolve({
@@ -89,7 +89,7 @@ export const addIssue = ({ invoice, description, rejected_qty,uom }) => {
   return new Promise(async (resolve, reject) => {
     try {
       const [result] = await db.query(
-        `insert into cust_rejection_issues (invoice_id,description,rejected_qty,uom_id,created_date,last_updated) values (${invoice},"${description}",${rejected_qty},${uom},"${curr_date}","${curr_date}");`
+        `insert into cust_rejection_issues (invoice_id,description,rejected_qty,uom_id,created_date,last_updated) values (${invoice},"${description}",${rejected_qty},${uom},NOW(),NOW());`
       );
       let insertedIssue = {
         id: result.insertId,
@@ -126,7 +126,7 @@ export const editIssue = ({ cust_rej_id, description, rejected_qty, uom }) => {
         `select * from cust_rejection_issues where cust_rej_id = ${cust_rej_id};`
       );
       const [result] = await db.query(
-        `update cust_rejection_issues set description = "${description}" , rejected_qty = ${rejected_qty},uom_id = ${uom} ,last_updated = "${curr_date}" where cust_rej_id = ${cust_rej_id};`
+        `update cust_rejection_issues set description = "${description}" , rejected_qty = ${rejected_qty},uom_id = ${uom} ,last_updated = NOW() where cust_rej_id = ${cust_rej_id};`
       );
       let editedIssue = {
         cust_rej_id,
@@ -173,7 +173,7 @@ export const addContainment = ({
   return new Promise(async (resolve, reject) => {
     try {
       const [result] = await db.query(
-        `insert into cust_containment_actions (cust_rej_id,stock_check_supplier,supplier_date,stock_check_customer, customer_date, stock_check_production, production_date, stock_check_transit, transit_date, remarks, created_date, last_updated) values (${issue},"${stock_check_supplier}","${supplier_date}","${stock_check_customer}","${customer_date}","${stock_check_production}","${production_date}","${stock_check_transit}","${transit_date}","${remarks}","${curr_date}","${curr_date}");`
+        `insert into cust_containment_actions (cust_rej_id,stock_check_supplier,supplier_date,stock_check_customer, customer_date, stock_check_production, production_date, stock_check_transit, transit_date, remarks, created_date, last_updated) values (${issue},"${stock_check_supplier}","${supplier_date}","${stock_check_customer}","${customer_date}","${stock_check_production}","${production_date}","${stock_check_transit}","${transit_date}","${remarks}",NOW(),NOW());`
       );
       let insertedContainment = {
         id: result.insertId,
@@ -228,7 +228,7 @@ export const editContainment = ({
         `select * from cust_containment_actions where containment_id = ${containment_id}`
       );
       const [result] = await db.query(
-        `update cust_containment_actions set stock_check_supplier = "${stock_check_supplier}", supplier_date = "${supplier_date}", stock_check_customer = "${stock_check_customer}", customer_date = "${customer_date}", stock_check_production = "${stock_check_production}", production_date = "${production_date}", stock_check_transit = "${stock_check_transit}", transit_date = "${transit_date}", remarks = "${remarks}", last_updated = ${curr_date} where containment_id = ${containment_id};`
+        `update cust_containment_actions set stock_check_supplier = "${stock_check_supplier}", supplier_date = "${supplier_date}", stock_check_customer = "${stock_check_customer}", customer_date = "${customer_date}", stock_check_production = "${stock_check_production}", production_date = "${production_date}", stock_check_transit = "${stock_check_transit}", transit_date = "${transit_date}", remarks = "${remarks}", last_updated = NOW() where containment_id = ${containment_id};`
       );
       let editedContainment = {
         containment_id,
@@ -284,7 +284,7 @@ export const addCorrective = ({ issue, inspector, description, remarks }) => {
   return new Promise(async (resolve, reject) => {
     try {
       const [result] = await db.query(
-        `insert into cust_corrective_actions (description,inspector_id,cust_rej_id,remarks,created_date,last_updated) values ("${description}",${inspector},${issue},"${remarks}","${curr_date}","${curr_date}");`
+        `insert into cust_corrective_actions (description,inspector_id,cust_rej_id,remarks,created_date,last_updated) values ("${description}",${inspector},${issue},"${remarks}",NOW(),NOW());`
       );
       let insertedCorrective = {
         id: result.insertId,
@@ -327,7 +327,7 @@ export const editCorrective = ({
         `select * from cust_corrective_actions where corrective_id = ${corrective_id};`
       );
       const [result] = await db.query(
-        `update cust_corrective_actions set inspector_id = ${inspector}, description = "${description}", remarks = "${remarks}", last_updated  = "${curr_date}" where corrective_id = ${corrective_id}`
+        `update cust_corrective_actions set inspector_id = ${inspector}, description = "${description}", remarks = "${remarks}", last_updated  = NOW() where corrective_id = ${corrective_id}`
       );
       let editedCorrectvie = {
         corrective_id,
@@ -363,7 +363,7 @@ export const addRootCause = ({ issue, description, remarks }) => {
   return new Promise(async (resolve, reject) => {
     try {
       const [result] = await db.query(
-        `insert into cust_root_causes (cust_rej_id,description,remarks,created_date,last_updated) values (${issue},"${description}","${remarks}","${curr_date}","${curr_date}");`
+        `insert into cust_root_causes (cust_rej_id,description,remarks,created_date,last_updated) values (${issue},"${description}","${remarks}",NOW(),NOW());`
       );
       let createdRootCause = {
         id: result.insertId,
@@ -400,7 +400,7 @@ export const editRootCause = ({ root_id, description, remarks }) => {
         `select * from cust_root_causes where root_id = ${root_id};`
       );
       const [result] = await db.query(
-        `update cust_root_causes set description = "${description}", remarks = "${remarks}", last_updated = "${curr_date}" where root_id = ${root_id};`
+        `update cust_root_causes set description = "${description}", remarks = "${remarks}", last_updated = NOW() where root_id = ${root_id};`
       );
       let editedRootCause = {
         root_id,
@@ -440,7 +440,7 @@ export const addPreventiveAction = ({
   return new Promise(async (resolve, reject) => {
     try {
       const [result] = await db.query(
-        `insert into cust_preventive_actions (cust_rej_id,description,inspector_id,remarks,created_date,last_updated) values (${issue},"${description}",${inspector},"${remarks}","${curr_date}","${curr_date}");`
+        `insert into cust_preventive_actions (cust_rej_id,description,inspector_id,remarks,created_date,last_updated) values (${issue},"${description}",${inspector},"${remarks}",NOW(),NOW());`
       );
 
       let addedPreventiveAction = {
@@ -479,7 +479,7 @@ export const editPreventiveAction = ({
   return new Promise(async(resolve,reject) => {
     try{
       const [[preventive_action]] = await db.query(`select * from cust_preventive_actions where preventive_id = ${preventive_id}`)
-      const [result] = await db.query(`update cust_preventive_actions set inspector_id = ${inspector}, description = "${description}",remarks = "${remarks}", last_updated = "${curr_date}" where preventive_id = ${preventive_id}`)
+      const [result] = await db.query(`update cust_preventive_actions set inspector_id = ${inspector}, description = "${description}",remarks = "${remarks}", last_updated = NOW() where preventive_id = ${preventive_id}`)
       let editedPreventiveAction = {
         preventive_id,
         inspector,
